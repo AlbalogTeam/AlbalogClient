de3078dcd9febfae2bccf76f41aa467bd55b9340
Alexis1226 <ahwr41@gmail.com>
2021-05-17 12:50:20 +0900

    Page of Login and SignUp ver0.1

--- albalog-client/src/App.js
+++ albalog-client/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Link} from 'react-router-dom';
+
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path ="/" component={Login} exact/>
+      <Route path ="/signup" component={SignUp} />
     </div>
   );
 }
---
+++ albalog-client/src/Components/SignUp.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './SignUp.scss';
+
+function SignUp() {
+    return (
+        <div>
+            <div className="signUpLeft">
+                <h1>Albalog</h1>
+                <span>이메일</span>
+                <input type="text" placeholder="내용을 입력해주세요"/>
+                <span>이름</span>
+                <input type="text" placeholder="내용을 입력해주세요"/>
+                <span>비밀번호</span>
+                <input type="password" placeholder="내용을 입력해주세요"/>
+                <span>비밀번호 확인</span>
+                <input type="password" placeholder="내용을 입력해주세요"/>
+                <button className="signUp">가입하기</button>
+            </div>
+            <div className="signUpRight">   
+                <label>
+                    <input type="checkbox" />
+                    <span>서비스 이용약관</span>
+                </label>
+                <div>
+                    <h5>제 1조 서비스이용약관</h5>
+                    <p>가나다라마바사아자차카타파하</p>
+                    
+                    <h5>제 2조 서비스이용약관</h5>
+                    <p>가나다라마바사아자차카타파하</p>
+                </div>
+                <label>
+                    <input type="checkbox" />
+                    <span>개인정보처리방침</span>
+                </label>
+                <div>
+                    <h5>제 1조 개인정보처리방침</h5>
+                    <p>가나다라마바사아자차카타파하</p>
+                    
+                    <h5>제 2조 개인정보처리방침</h5>
+                    <p>가나다라마바사아자차카타파하</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignUp;
