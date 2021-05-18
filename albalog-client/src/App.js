556f3bd2a36e0cc74abef2f56943decc03522e24
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-18 10:51:33 +0900

    Styles : markup Notice Page

--- albalog-client/src/App.js
+++ albalog-client/src/App.js
@@ -1,10 +1,14 @@
 import AdminHeader from 'components/admin/AdminHeader/AdminHeader';
+import Notice from 'pages/Notice/Notice';
 import React from 'react';
 
 const App = () => {
   return (
     <div>
       <AdminHeader />
+      <div id="container">
+        <Notice />
+      </div>
     </div>
   );
 };
---
+++ albalog-client/src/pages/Notice/Notice.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import './Notice.scss';
+import {
+  AiOutlineDoubleLeft,
+  AiOutlineLeft,
+  AiOutlineRight,
+  AiOutlineDoubleRight,
+} from 'react-icons/ai';
+
+const Notice = () => {
+  return (
+    <div id="Notice">
+      <div className="notice-tit">
+        <h4 className="tit-corp">공지사항</h4>
+      </div>
+      <div className="notice-cont">
+        <div className="table-comm">
+          <table className="table">
+            <colgroup>
+              <col style={{ width: '81%' }} />
+              <col style={{ width: '19%' }} />
+            </colgroup>
+            <thead>
+              <tr>
+                <th scope="col">내용</th>
+                <th scope="col">등록일</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="td-left">
+                  <div className="inner-cont">
+                    <span className="inner-text">
+                      <a href="" className="link-text">
+                        5월달 할인 이벤트 종류 입니다.
+                      </a>
+                    </span>
+                  </div>
+                </td>
+                <td>
+                  <div className="inner-cont">2021-05-19</div>
+                </td>
+              </tr>
+
+              <tr>
+                <td className="td-left">
+                  <div className="inner-cont">
+                    <span className="inner-text">
+                      <a href="" className="link-text">
+                        5월달 할인 이벤트 종류 입니다.
+                      </a>
+                    </span>
+                  </div>
+                </td>
+                <td>
+                  <div className="inner-cont">2021-05-19</div>
+                </td>
+              </tr>
+
+              <tr>
+                <td className="td-left">
+                  <div className="inner-cont">
+                    <span className="inner-text">
+                      <a href="" className="link-text">
+                        5월달 할인 이벤트 종류 입니다.
+                      </a>
+                    </span>
+                  </div>
+                </td>
+                <td>
+                  <div className="inner-cont">2021-05-19</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="paging-comm">
+          <div className="inner-paging">
+            <a href="" className="paging-btn paging-fst">
+              <AiOutlineDoubleLeft size="14" />
+            </a>
+            <a href="" className="paging-btn paging-prev">
+              <AiOutlineLeft size="14" />
+            </a>
+            <a href="" className="paging-link active">
+              1
+            </a>
+            <a href="" className="paging-link">
+              2
+            </a>
+            <a href="" className="paging-link">
+              3
+            </a>
+            <a href="" className="paging-btn paging-next">
+              <AiOutlineRight size="14" />
+            </a>
+            <a href="" className="paging-btn paging-last">
+              <AiOutlineDoubleRight size="14" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Notice;
