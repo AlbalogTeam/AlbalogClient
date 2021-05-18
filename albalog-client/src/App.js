8af17cc5e9fc78f366f4c7f69aed2e79cc9619b2
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-18 15:49:35 +0900

    add : react-router-dom

--- albalog-client/src/App.js
+++ albalog-client/src/App.js
@@ -1,13 +1,18 @@
 import AdminHeader from 'components/admin/AdminHeader/AdminHeader';
 import Notice from 'pages/notice/Notice';
+import NoticeDetail from 'pages/noticeDetail/NoticeDetail';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
+    <div id="container">
       <AdminHeader />
-      <div id="container">
-        <Notice />
+      <div id="main">
+        <Switch>
+          <Route path="/notice" exact component={Notice} />
+          <Route path="/notice/:id?" exact component={NoticeDetail} />
+        </Switch>
       </div>
     </div>
   );
