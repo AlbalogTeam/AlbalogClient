d5b294af58e80836a57802f3d2683c34657e74c9
yoonyounghoon <yunyounghoon23@gmail.com>
2021-05-24 17:07:28 +0900

    Implements  : 페이지 라우팅 설정

--- albalog-client/src/App.js
+++ albalog-client/src/App.js
@@ -1,11 +1,27 @@
+import AdminAside from 'components/admin/AdminAside/AdminAside';
 import AdminHeader from 'components/admin/AdminHeader/AdminHeader';
+import AdminInfo from 'components/admin/AdminInfo/AdminInfo';
+import AdminDashboardPage from 'pages/admin/AdminDashboardPage';
+import AdminEmployeePage from 'pages/admin/AdminEmployeePage';
+import AdminInfoPage from 'pages/admin/AdminInfoPage';
+import AdminPayrollPage from 'pages/admin/AdminPayrollPage';
+import AdminSchedulePage from 'pages/admin/AdminSchedulePage';
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <AdminHeader />
-    </div>
+      <AdminAside />
+      <Switch>
+        <Route path="/" exact component={AdminDashboardPage} />
+        <Route path="/admin/info" component={AdminInfoPage} />
+        <Route path="/admin/payroll" component={AdminPayrollPage} />
+        <Route path="/admin/employeelist" component={AdminEmployeePage} />
+        <Route path="/admin/schedule" component={AdminSchedulePage} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
