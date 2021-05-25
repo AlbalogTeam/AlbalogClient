af59e45225a9ce2a5c319648e2d3540192d4968b
Alexis1226 <ahwr41@gmail.com>
2021-05-25 23:54:29 +0900

    Refactor : loading data

--- albalog-client/src/Components/partTime/dashboard/DashboardNotice.js
+++ albalog-client/src/Components/partTime/dashboard/DashboardNotice.js
@@ -3,40 +3,41 @@ import axios from 'axios';
 import 'components/partTime/dashboard/DashboardNotice.scss';
 
 function DashboardNotice() {
-  const [sample, setSample] = useState(null);
+  const [samples, setSamples] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts/')
-      .then((response) => response.data)
-      .then((data) => {
-        console.log(data);
-        setSample(data);
-      });
+    //   axios
+    //     .get('https://jsonplaceholder.typicode.com/posts/')
+    //     .then((response) => response.data)
+    //     .then((data) => {
+    //       console.log(data);
+    //       setSamples(data);
+    //     });
+    // }, []);
+
+    const fetchSamples = async () => {
+      try {
+        setError(null);
+        // setSamples(null);
+        setLoading(true);
+        let response = await axios.get(
+          'https://jsonplaceholder.typicode.com/posts',
+        );
+        console.log(response.data);
+        setSamples(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    fetchSamples();
   }, []);
 
-  //   const fetchSample = async () => {
-  //     try {
-  //       setError(null);
-  //       setSample(null);
-  //       setLoading(true);
-  //       let response = await axios.get(
-  //         'https://jsonplaceholder.typicode.com/posts/1',
-  //       );
-  //       setSample(response.data);
-  //     } catch (e) {
-  //       setError(e);
-  //     }
-  //     setLoading(false);
-  //   };
-  //   fetchSample();
-  // }, []);
-
-  // if (loading) return <div>로딩중..</div>;
-  // if (error) return <div>에러가 발생했습니다 {console.log(error)}</div>;
-  // if (!sample) return <div>{console.log(sample)}</div>;
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다 {console.log(error)}</div>;
+  if (!samples) return <div>{console.log(samples)}</div>;
 
   return (
     <div id="DashboardNotice">
@@ -45,17 +46,16 @@ function DashboardNotice() {
         <div className="td2-head">제목</div>
         <div className="td3-head">작성자</div>
       </div>
-      {/* {post()} */}
-      {sample.map((data) => {
-        if (data.id < 3)
-          return (
+      {samples &&
+        samples
+          .filter((x) => x.id > 3)
+          .map((data) => (
             <div className="tr">
               <div className="td1">{data.id}</div>
               <div className="td2">{data.title}</div>
               <div className="td3">{data.userId}</div>
             </div>
-          );
-      })}
+          ))}
     </div>
   );
 }
