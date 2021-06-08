0a187df63495ab13a75c99f9397f1d679138500f
D.seok <ehskstkrjsa@naver.com>
2021-06-08 20:14:00 +0900

    회원가입 html/css파일 업로드

---
+++ albalog-client/src/components/SignUp2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignUp2.scss';
+
+function SignUp2(){
+    return(
+        <div id="SignUp2">
+            
+        </div>
+    )
+}
+
+export default SignUp2;
