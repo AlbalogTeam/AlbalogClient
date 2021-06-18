e1fb8a785c523b8063ebac1d6680f996d0687834
Alexis1226 <ahwr41@gmail.com>
2021-06-18 18:18:36 +0900

    Refactor : staff dashboard

--- albalog-client/src/components/partTime/dashboard/DashboardNotice.js
+++ albalog-client/src/components/partTime/dashboard/DashboardNotice.js
@@ -1,46 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import 'components/partTime/dashboard/DashboardNotice.scss';
 import { useSelector } from 'react-redux';
-import client from 'utils/api';
-import axios from 'axios';
 
 function DashboardNotice() {
-  // const [samples, setSamples] = useState([]);
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(null);
   const shop = useSelector((state) => state.shop);
-  console.log(shop);
   const notices = shop.notices;
-  // useEffect(() => {
-
-  //   axios
-  //     .get('https://jsonplaceholder.typicode.com/posts/')
-  //     .then((response) => response.data)
-  //     .then((data) => {
-  //       console.log(data);
-  //       setSamples(data);
-  //     });
-  // }, []);
-
-  //   const fetchSamples = async () => {
-  //     try {
-  //       setError(null);
-  //       // setSamples(null);
-  //       setLoading(true);
-  //       let response = await axios.get(`/${shop._id}/notice`);
-  //       console.log('공지사항' + response.data);
-  //       setSamples(response.data.notices);
-  //     } catch (e) {
-  //       setError(e);
-  //     }
-  //     setLoading(false);
-  //   };
-  //   fetchSamples();
-  // }, []);
-
-  // if (loading) return <div>로딩중..</div>;
-  // if (error) return <div>에러가 발생했습니다 {console.log(error)}</div>;
-  // if (!samples) return <div>{console.log(samples)}</div>;
 
   return (
     <div id="DashboardNotice">
