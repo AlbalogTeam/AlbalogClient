e2286d68340a4d749c0f80a4572bf64cbfa9166e
yoonyounghoon <yunyounghoon23@gmail.com>
2021-07-02 16:56:09 +0900

    redux-saga 미들웨어 설정, redux date 모듈 추가

---
+++ albalog-client/src/modules/date.js
@@ -0,0 +1,90 @@
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit';
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { getMonthDataAPI } from 'utils/api/date';
+
+// 액션 타입 정의
+const GET_MONTH_DATA = 'date/GET_MONTH_DATA';
+const GET_PREV_MONTH = 'date/GET_PREV_MONTH';
+const GET_PREV_MONTH_SUCCESS = 'date/GET_PREV_MONTH_SUCCESS';
+const GET_PREV_MONTH_FAILURE = 'date/GET_PREV_MONTH_FAILURE';
+const GET_NEXT_MONTH = 'date/GET_NEXT_MONTH';
+const GET_NEXT_MONTH_SUCCESS = 'date/GET_NEXT_MONTH_SUCCESS';
+
+// 액션 생성 함수 정의
+export const getMonthData = createAction(GET_MONTH_DATA);
+export const getPrevMonth = createAction(GET_PREV_MONTH);
+export const getNextMonth = createAction(GET_NEXT_MONTH);
+
+export function* dateSaga() {
+  yield takeEvery(GET_PREV_MONTH, getPrevMonthSaga);
+  yield takeEvery(GET_NEXT_MONTH, getNextMonthSaga);
+}
+
+export function* getPrevMonthSaga(action) {
+  // 날짜 설정
+  let { year, month, shopId } = action.payload;
+  if (month === 1) {
+    year -= 1;
+    month = 12;
+  } else {
+    month -= 1;
+  }
+  try {
+    const response = yield call(getMonthDataAPI, { year, month, shopId });
+    yield put({
+      type: GET_PREV_MONTH_SUCCESS,
+      payload: { year, month, response },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export function* getNextMonthSaga(action) {
+  let { year, month, shopId } = action.payload;
+  if (month === 12) {
+    year += 1;
+    month = 1;
+  } else {
+    month += 1;
+  }
+  try {
+    const response = yield call(getMonthDataAPI, { year, month, shopId });
+    yield put({
+      type: GET_NEXT_MONTH_SUCCESS,
+      payload: { year, month, response },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const initialState = {
+  year: new Date().getFullYear(),
+  month: new Date().getMonth() + 1,
+  payrollData: [],
+};
+
+const date = createReducer(initialState, {
+  [GET_PREV_MONTH]: (state, action) => ({
+    ...state,
+  }),
+  [GET_PREV_MONTH_SUCCESS]: (state, action) => ({
+    ...state,
+    year: action.payload.year,
+    month: action.payload.month,
+    payrollData: action.payload.response,
+  }),
+
+  [GET_NEXT_MONTH]: (state, action) => ({
+    ...state,
+  }),
+  [GET_NEXT_MONTH_SUCCESS]: (state, action) => ({
+    ...state,
+    year: action.payload.year,
+    month: action.payload.month,
+    payrollData: action.payload.response,
+  }),
+});
+
+export default date;
--- albalog-client/src/modules/index.js
+++ albalog-client/src/modules/index.js
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { all } from 'redux-saga/effects';
 import auth from './auth';
-
+import { dateSaga } from './date';
 import parttime from './parttime';
 import render from './render';
 import shop from './shop';
 import transition from './transition';
 import user from './user';
 import workManual from './workManual';
+import date from './date';
 
 const rootReducer = combineReducers({
   auth: auth.reducer,
@@ -17,10 +20,18 @@ const rootReducer = combineReducers({
   parttime: parttime.reducer,
   transition: transition.reducer,
   render: render.reducer,
+  date,
 });
+
+export const sagaMiddleware = createSagaMiddleware();
+
+export function* rootSaga() {
+  yield all([dateSaga()]);
+}
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [sagaMiddleware],
 });
 
 export default store;
