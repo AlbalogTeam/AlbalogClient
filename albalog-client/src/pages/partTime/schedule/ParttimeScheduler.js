28e3a1add403f5d989d315f2dcb6e53d09ca6433
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-07-03 21:08:07 +0900

    Fix : error 해결

--- albalog-client/src/pages/partTime/schedule/ParttimeScheduler.js
+++ albalog-client/src/pages/partTime/schedule/ParttimeScheduler.js
@@ -41,10 +41,10 @@ function ParttimeScheduler() {
 
   const getPersonalSchedule = async () => {
     try {
-      const response = await client.get(`${APIURL}/shift/employee/${user._id}`);
+      const response = await client.get(`/shift/employee/${user._id}`);
       let shift = await response.data.map((a) => {
-        const st = new Date(new Date(a.start).getTime());
-        const ed = new Date(new Date(a.end).getTime()); //540 * 60 * 1000
+        const st = new Date(new Date(a.start).getTime() - 540 * 60 * 1000);
+        const ed = new Date(new Date(a.end).getTime() - 540 * 60 * 1000); //540 * 60 * 1000
 
         let newData = {
           title: user.name,
@@ -59,12 +59,15 @@ function ParttimeScheduler() {
 
   const getAllSchedule = async () => {
     try {
-      const response = await client.get(`${APIURL}/shift/location/${shop._id}`);
+      const response = await client.get(`/shift/location/${shop._id}`);
+      console.log(response);
       let shift = await response.data.map((a) => {
+        const st = new Date(new Date(a.start).getTime() - 540 * 60 * 1000);
+        const ed = new Date(new Date(a.end).getTime() - 540 * 60 * 1000); //540 * 60 * 1000
         let newData = {
           title: a.title,
-          start: new Date(a.start),
-          end: new Date(a.end),
+          start: new Date(st),
+          end: new Date(ed),
         };
         return newData;
       });
@@ -75,7 +78,7 @@ function ParttimeScheduler() {
   useEffect(() => {
     getPersonalSchedule();
     getAllSchedule();
-  }, []);
+  }, [shop]);
 
   const onChange = (e) => {
     e.target.value === 'personal' && setSelectedRadio('personal');
