2e1a16cc6fd7ff5d9d4ecc96c3d8ba2b9694601f
Alexis1226 <ahwr41@gmail.com>
2021-07-12 11:50:16 +0900

    Fix : date error in WorkingTime page

--- albalog-client/src/pages/partTime/WorkingTime.js
+++ albalog-client/src/pages/partTime/WorkingTime.js
@@ -6,19 +6,23 @@ import Header from 'components/Header/Header';
 import Aside from 'components/Aside/Aside';
 import Footer from 'components/Footer/Footer';
 import { useSelector } from 'react-redux';
+import { IoTodayOutline } from 'react-icons/io5';
 
 function WorkingTime() {
   const year = new Date().getFullYear();
-  const month = new Date().getMonth() + 1;
-  const date = new Date().getDate();
-  const [today, setToday] = useState(new Date(year, month - 1, date));
+  const month = new Date().getMonth();
+  const [today, setToday] = useState(new Date(year, month));
   const payrolls = useSelector((state) => state.parttime.payrolls);
 
   function filteredPayroll() {
     const monthlyPayroll =
       payrolls &&
-      payrolls.filter((a) => Number(a.yearAndMonth) === today.toString());
-    return !!monthlyPayroll ? monthlyPayroll[0].timeClock : 0;
+      payrolls.filter(
+        (a) =>
+          (a.yearAndMonth.toString().slice(0, 4) * 1 === today.getFullYear()) &
+          (a.yearAndMonth.toString().slice(4) * 1 === today.getMonth()),
+      );
+    return !!monthlyPayroll[0] ? monthlyPayroll[0].timeClock : 0;
   }
 
   const totalWorkingtime = filteredPayroll()
@@ -28,10 +32,11 @@ function WorkingTime() {
     : 0;
 
   const onClickLeft = () => {
-    setToday(new Date(today.setDate(today.getMonth - 1)));
+    setToday(new Date(today.setMonth(today.getMonth() - 1)));
   };
+
   const onClickRight = () => {
-    setToday(new Date(today.setDate(today.getMonth + 1)));
+    setToday(new Date(today.setMonth(today.getMonth() + 1)));
   };
 
   return (
