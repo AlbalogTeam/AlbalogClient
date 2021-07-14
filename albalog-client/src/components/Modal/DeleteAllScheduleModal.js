6d27d9e396a46d0082d7bc05c8354768c286044d
yoonyounghoon <yunyounghoon23@gmail.com>
2021-07-14 16:55:50 +0900

    Implements: 스케줄 삭제 기능 추가

---
+++ albalog-client/src/components/Modal/DeleteAllScheduleModal.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import client from 'utils/api';
+import './DeleteAllScheduleModal.scss';
+
+const DeleteAllScheduleModal = ({
+  handleDeleteModal,
+  employeeList,
+  locationId,
+}) => {
+  const [id, setId] = useState('');
+
+  const handleChangeId = (e) => {
+    setId(e.target.value);
+  };
+  console.log(employeeList);
+
+  const handleDeleteScheduleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await client.delete(
+        `/shift/location/${locationId}/employee/${id}/deleteAll`,
+      );
+      handleDeleteModal();
+      if (response.status === 200) {
+        window.location.replace(`/admin/${locationId}/schedule`); // 페이지 이동 후 새로고침
+      }
+    } catch (e) {
+      console.error(e);
+      alert('다시 시도해 주세요');
+    }
+  };
+
+  return (
+    <div className="del-modal-container">
+      <div className="del-modal-wrap">
+        <h2 className="title">해당 직원 스케줄 전체 삭제</h2>
+        <form onSubmit={handleDeleteScheduleSubmit}>
+          <label>직원선택</label>
+          <select onChange={handleChangeId}>
+            <option>직원을 선택해주세요</option>
+            {employeeList.map((employee) => (
+              <option key={employee.employee._id} value={employee.employee._id}>
+                {employee.employee.name}
+              </option>
+            ))}
+          </select>
+          <button className="btn-del" onClick={handleDeleteScheduleSubmit}>
+            삭제하기
+          </button>
+          <button className="btn-close" onClick={handleDeleteModal}>
+            닫기
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteAllScheduleModal;
--- albalog-client/src/components/admin/AdminSchedule/AdminSchedule.js
+++ albalog-client/src/components/admin/AdminSchedule/AdminSchedule.js
@@ -8,36 +8,72 @@ import './AdminSchedule.scss';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import client from 'utils/api';
+import DeleteAllScheduleModal from 'components/Modal/DeleteAllScheduleModal';
+import ScheduleInfoModal from 'components/Modal/ScheduleInfoModal';
 
 const localizer = momentLocalizer(moment);
 
 const AdminSchedule = () => {
   const employeeList = useSelector(({ shop }) => shop.employees);
   const [isModal, setIsModal] = useState(false);
+  const [deleteModalOpened, setDeleteModalOpened] = useState(false);
+  const [infoModalOpened, setInfoModalOpened] = useState(false);
   const locationId = useSelector(({ shop }) => shop._id);
   const [events, setEvents] = useState([]);
+  const [employee, setEmployee] = useState('');
 
   const handleModal = () => {
     setIsModal(!isModal);
   };
 
+  const handleDeleteModal = (e) => {
+    setDeleteModalOpened(!deleteModalOpened);
+  };
+
+  const handleInfoModal = async (e) => {
+    console.log(e);
+    setEmployee(e);
+    setInfoModalOpened(!infoModalOpened);
+  };
+
   const getAllSchedule = async () => {
     try {
       const response = await client.get(`/shift/location/${locationId}`);
-
-      // setEvents(response.data);
-      const newEvents = response.data.map((d) => {
-        const ddd = {
-          title: d.title,
-          start: new Date(new Date(d.start).getTime() - 540 * 60 * 1000),
-          end: new Date(new Date(d.end).getTime() - 540 * 60 * 1000),
+      const newEvents = response.data.map((employee) => {
+        const schedule = {
+          title: employee.title,
+          start: new Date(new Date(employee.start).getTime() - 540 * 60 * 1000),
+          end: new Date(new Date(employee.end).getTime() - 540 * 60 * 1000),
+          index: employee._id,
+          staffId: employee.staffId,
         };
-        return ddd;
+        return schedule;
       });
+
       setEvents(newEvents);
     } catch (e) {
       console.error(e);
     }
+  };
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    let key = '';
+    for (let i = 0; i < event.title.length; i++) {
+      key += event.title.charCodeAt(i);
+    }
+    let color = String(key.toString(16)).substr(9, 6);
+    let backgroundColor = '#' + color;
+    let style = {
+      backgroundColor: backgroundColor,
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+      border: '0px',
+      display: 'block',
+    };
+    return {
+      style: style,
+    };
   };
 
   useEffect(() => {
@@ -51,16 +87,15 @@ const AdminSchedule = () => {
       <div className="AdminSchedule">
         <div className="schedule-header">
           <h2 className="schedule-title">스케줄 페이지</h2>
-          <button className="btn-add" onClick={handleModal}>
-            스케줄 추가
-          </button>
+          <div>
+            <button className="btn-add" onClick={handleModal}>
+              스케줄 추가
+            </button>
+            <button className="btn-delAll" onClick={handleDeleteModal}>
+              스케줄 삭제
+            </button>
+          </div>
         </div>
-        {isModal && (
-          <ScheduleModal
-            handleModal={handleModal}
-            employeeList={employeeList}
-          />
-        )}
         <div className="schedule-calendar">
           <Calendar
             defaultView={'month'}
@@ -71,12 +106,28 @@ const AdminSchedule = () => {
             startAccessor="start"
             endAccessor="end"
             step={30}
-            onSelectEvent={(event, e) => {
-              console.log(event);
-            }}
+            eventPropGetter={eventStyleGetter}
+            onSelectEvent={handleInfoModal}
           />
         </div>
       </div>
+      {isModal && (
+        <ScheduleModal handleModal={handleModal} employeeList={employeeList} />
+      )}
+      {deleteModalOpened && (
+        <DeleteAllScheduleModal
+          handleDeleteModal={handleDeleteModal}
+          employeeList={employeeList}
+          locationId={locationId}
+        />
+      )}
+      {infoModalOpened && (
+        <ScheduleInfoModal
+          handleInfoModal={handleInfoModal}
+          employee={employee}
+          locationId={locationId}
+        />
+      )}
     </>
   );
 };
