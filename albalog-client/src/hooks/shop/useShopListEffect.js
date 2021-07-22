950bf0f672ac7e45b1601f2bfdcac7d8ef59e899
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-07-22 22:41:33 +0900

    Refector : 매장 수정 리팩토링

---
+++ albalog-client/src/hooks/shop/useShopListEffect.js
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getShopListForOwner, getShopListForParttime } from 'utils/api/shop';
+
+export default function useShopListEffect() {
+  const [locations, setLocations] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const user = useSelector((state) => state.user);
+
+  const getDataForOwner = async () => {
+    try {
+      setLoading(true);
+      const locations = await getShopListForOwner();
+      setLocations(locations);
+    } catch (e) {
+      alert('매장 리스트를 불러오는데 실패했습니다.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getDataForParttime = async () => {
+    try {
+      setLoading(true);
+      const locations = await getShopListForParttime();
+      setLocations(locations);
+    } catch (e) {
+      alert('매장 리스트를 불러오는데 실패했습니다.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.role === 'owner') getDataForOwner();
+    if (user.role === 'staff') getDataForParttime();
+  }, [user.role]);
+
+  return {
+    locations,
+    loading,
+    user,
+  };
+}
